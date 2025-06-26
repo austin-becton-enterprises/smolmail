e09c2bcbc37c6271ea4d3dea583a3192341ee1ff
Kiran BIswa <kiran.biswa682@gmail.com>
2025-06-26 13:22:48 -0400

    added some code to read-email

--- agents/Tools/read_email.py
+++ agents/Tools/read_email.py
@@ -2,13 +2,23 @@
 Tool: EmailReader
 Purpose: Parses and understands emails from an inbox
 """
+from smolagents.tool import Tool
+# this is where you'll define your test emails
 
-class EmailReader:
-    def __init__(self, emails): 
-        self.emails = emails
+
+class EmailReader(Tool):
+    name = "read_email"
+    description = "Reads emails from a specificed inboxed and returns them."
+    input = {}
+    output_type = "string"
 
     def forward(self): 
         # return email, mpossibly formatted (email, subject, body) or
-        # return just the relevant information
+        # return just the relevanpipt information
+
+        # *** WE WILL NOT NEED TO CREATE A DUMMY_DATA, THE DATA TEAM ALREADY HAS,
+        # *** WE CAN EITHER GO THROUGH THEIR CODE AND MAKE SURE WE REFERENCE THE CORRECT DATA
+        # *** OR WE CAN MESSAGE LIANA OR SIMRAN ABOUT IT
+        
         return 0 
     
