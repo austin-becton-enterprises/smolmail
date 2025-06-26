c7b8f59cba4a6c8cffdbb19986cc9d0596f619a9
Kiran BIswa <kiran.biswa682@gmail.com>
2025-06-26 12:04:07 -0400

    set up files for tools

---
+++ agents/Tools/read_email.py
@@ -0,0 +1,14 @@
+"""
+Tool: EmailReader
+Purpose: Parses and understands emails from an inbox
+"""
+
+class EmailReader:
+    def __init__(self, emails): 
+        self.emails = emails
+
+    def forward(self): 
+        # return email, mpossibly formatted (email, subject, body) or
+        # return just the relevant information
+        return 0 
+    
---
+++ agents/Tools/send_email.py
@@ -0,0 +1,10 @@
+"""
+Tool: EmailSender
+Purpose: writes and sends email on behalf of the user 
+"""
+
+class EmailSender(): 
+
+
+    def forward(): 
+        return 0
